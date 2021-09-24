b7fd9ee5414f14510fd382f1f13502b5bb5888f2
YE <yet@ego.team>
2021-09-24 12:00:18 +0300

    Add dummy Emacs package

---
+++ dummy.el
@@ -0,0 +1,17 @@
+;;; dummy.el --- A dummy Emacs package
+
+;; Copyright (C) 2021 Free Software Foundation, Inc.
+
+;; Author: Y. E. <nowhere@example.com>
+;; Version: 0.1
+
+;;; Commentary:
+
+;; This is a dummy package.
+
+(defun say-hello ()
+  "Greet a stranger."
+  (message "Hello Stranger!"))
+
+(provide 'dummy)
+;;; dummy.el ends here
